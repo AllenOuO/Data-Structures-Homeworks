fd28fc3326f6577a026b70f7229ea74a29bd154e
AllenOuO <113157947+AllenOuO@users.noreply.github.com>
2025-07-20 14:26:58 +0800

    Update Ackermann-NonRecursive.cpp

--- homework1/src/problem1/Ackermann-NonRecursive.cpp
+++ homework1/src/problem1/Ackermann-NonRecursive.cpp
@@ -1,26 +1,25 @@
 #include<iostream>
 
 int A(int m,int n){
-	int a[100000];
-	int i = 0;
-	while(1){
-		if(0 == m){
-			if(0==i){
-				return ++n;
-			}
-			n++;
-			m=a[i--];
-		}
-		if(0 == n){
-			m--;
-			n++;
-		}
-
-		if( 0 != m && 0 != n){
-			a[++i]=m-1;
-			n--;
-		}
-	}
+    int a[100000];
+    int i = 0;
+    while(1){
+        if(0 == m){
+            if(0==i){
+                return ++n;
+	    }
+	    n++;
+            m=a[i--];
+        }
+        if(0 == n){
+            m--;
+            n++;
+        }
+        if( 0 != m && 0 != n){
+            a[++i]=m-1;
+            n--;
+        }
+    }
 }
 
 int main(){
