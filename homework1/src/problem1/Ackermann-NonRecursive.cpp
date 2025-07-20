46dd34b4d6e8aed6c2511595c3d4720f2a7ef699
AllenOuO <113157947+AllenOuO@users.noreply.github.com>
2025-07-20 14:24:07 +0800

    Add files via upload

--- homework1/src/problem1/Ackermann-NonRecursive.cpp
+++ homework1/src/problem1/Ackermann-NonRecursive.cpp
@@ -1,48 +1,31 @@
-#include <iostream>
-#include <stack>
+#include<iostream>
 
-int A(int m, int n) {
-    std::stack<int> s1;
-    std::stack<int> s2;
-    s1.push(m);
-    s2.push(n);
+int A(int m,int n){
+	int a[100000];
+	int i = 0;
+	while(1){
+		if(0 == m){
+			if(0==i){
+				return ++n;
+			}
+			n++;
+			m=a[i--];
+		}
+		if(0 == n){
+			m--;
+			n++;
+		}
 
-    while (!s1.empty())
-    {
-        while (m != 0)
-        {
-            if (n == 0)
-            {
-                m = m - 1;
-                n = 1;
-                s1.push(m);
-                s2.push(n);
-            }
-            else
-            {
-                n = n - 1;
-                s1.push(m - 1);
-                s2.push(-1);
-            }
-        }
-        n++;
-        while (!s1.empty() && s2.top() != -1) {
-            s1.pop();
-            s2.pop();
-        }
-
-        if (!s1.empty()) {
-            m = s1.top();
-            s2.pop();
-            s2.push(n);
-        }
-    }
-    return n;
+		if( 0 != m && 0 != n){
+			a[++i]=m-1;
+			n--;
+		}
+	}
 }
 
-int main() {
+int main(){
     int m, n;
-    while(1) {
+    while(1){
         std::cout << "Enter m and n: ";
         std::cin >> m >> n;
         std::cout << "A(" << m << ", " << n << ") = " << A(m, n) << std::endl;
