5b574a6c8e4cf85dc322cc9ec3bb43b341fa4033
AllenOuO <113157947+AllenOuO@users.noreply.github.com>
2025-07-20 14:18:39 +0800

    Add files via upload

--- homework1/src/problem1/Ackermann-Recursive.cpp
+++ homework1/src/problem1/Ackermann-Recursive.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
-int A(int m, int n) {
+int A(int m, int n);
+int A(int m, int n){
     if (m == 0) return n + 1;
     else if (n == 0) return A(m - 1, 1);
     else return A(m - 1, A(m, n - 1));
 }
 
-int main() {
+int main(){
     int m, n;
     while(1) {
         std::cout << "Enter m and n: ";
