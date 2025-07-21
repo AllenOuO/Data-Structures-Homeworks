a2a4a3c7f8aa4db1d8bb1728ad8b24454e1c5972
AllenOuO <113157947+AllenOuO@users.noreply.github.com>
2025-07-21 21:01:56 +0800

    Update Powerset.cpp

--- homework1/src/problem2/Powerset.cpp
+++ homework1/src/problem2/Powerset.cpp
@@ -4,29 +4,29 @@
 
 int main(){
     std::string line;
-    while(1){
-        std::cout << "Input: ";
-        getline(std::cin, line);
-        std::stringstream ss(line);
-        std::string token;
-        std::string subs[100];
-        int n = 0;
-        while(ss >> token) 
-            subs[n++] = token;
+    std::cout << "Input: ";
+    getline(std::cin, line);
+    std::stringstream ss(line);
+    std::string token;
+    std::string subs[100];
+    int n = 0;
+    while(ss >> token) 
+        subs[n++] = token;
+    std::cout << "{";
+    bool a = true;
+    int total = 1 << n;
+    for(int i = 0; i < total; ++i)
+    {
+        if(!a) 
+            std::cout << ", ";
         std::cout << "{";
-        bool a = true;
-        int total = 1 << n;
-        for(int i = 0; i < total; ++i) {
-            if(!a) 
-                std::cout << ", ";
-            std::cout << "{";
-        for(int j = 0; j < n; ++j) {
-                if (i & (1 << j))
-                 std::cout << subs[j];
-        }
-        std::cout << "}";
-        a = false;
-        }
-        std::cout << "}" << std::endl;
+    for(int j = 0; j < n; ++j)
+    {
+        if (i & (1 << j))
+            std::cout << subs[j];
     }
+    std::cout << "}";
+    a = false;
+    }
+    std::cout << "}" << std::endl;
 }
